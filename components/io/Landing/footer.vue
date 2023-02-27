2f0e698f40fcb40a66154c017eaf15802e5711c8
Ahmad Essam <61994278+AHMAD19858@users.noreply.github.com>
2023-02-27 14:29:34 +0200

    footer content

--- components/io/Landing/footer.vue
+++ components/io/Landing/footer.vue
@@ -1,80 +1,88 @@
 <template>
+	<div>
 
-  <div>
-<!--     <div class="card">
-   
+		<div class=" relative">
+			<div class="card">
 
 
-  
 
-</div> -->
 
-    <footer class="p-6 text-white z-10 relative bg-gradient-to-r from-[#FF6137] to-[#CC4D2C] ">
- 
-        <div class="container grid grid-cols-2 mx-auto gap-x-3 gap-y-8 sm:grid-cols-3 md:grid-cols-4">
-			<div class="flex flex-col space-y-4">
-				<h2 class=" font-semibold text-[26px]">FEATURES</h2>
-				<div class="flex flex-col space-y-2 text-sm text-white">
-					<a rel="noopener noreferrer" href="#">Installation</a>
-					<a rel="noopener noreferrer" href="#">Release Notes</a>
-					<a rel="noopener noreferrer" href="#">Upgrade Guide</a>
-					<a rel="noopener noreferrer" href="#">Using with Preprocessors</a>
-					<a rel="noopener noreferrer" href="#">Optimizing for Production</a>
-					<a rel="noopener noreferrer" href="#">Browser Support</a>
-					<a rel="noopener noreferrer" href="#">IntelliSense</a>
-				</div>
-			</div>
-			<div class="flex flex-col space-y-4">
-				<h2 class="f font-semibold text-[26px]">RESTAURANT TYPES</h2>
-				<div class="flex flex-col space-y-2 text-sm text-white">
-					<a rel="noopener noreferrer" href="#">Utility-First</a>
-					<a rel="noopener noreferrer" href="#">Responsive Design</a>
-					<a rel="noopener noreferrer" href="#">Hover, Focus, &amp; Other States</a>
-					<a rel="noopener noreferrer" href="#">Dark Mode</a>
-					<a rel="noopener noreferrer" href="#">Adding Base Styles</a>
-					<a rel="noopener noreferrer" href="#">Extracting Components</a>
-					<a rel="noopener noreferrer" href="#">Adding New Utilities</a>
-				</div>
-			</div>
-			<div class="flex flex-col space-y-4">
-				<h2 class=" font-semibold text-[26px]">COMPANY</h2>
-				<div class="flex flex-col space-y-2 text-sm text-white">
-					<a rel="noopener noreferrer" href="#">Configuration</a>
-					<a rel="noopener noreferrer" href="#">Theme Configuration</a>
-					<a rel="noopener noreferrer" href="#">Breakpoints</a>
-					<a rel="noopener noreferrer" href="#">Customizing Colors</a>
-					<a rel="noopener noreferrer" href="#">Customizing Spacing</a>
-					<a rel="noopener noreferrer" href="#">Configuring Variants</a>
-					<a rel="noopener noreferrer" href="#">Plugins</a>
-				</div>
-			</div>
-			<div class="flex flex-col space-y-4">
-				<h2 class=" font-semibold text-[26px]">Contact</h2>
-				<div class="flex flex-col space-y-2 text-sm text-white">
-					<a rel="noopener noreferrer" href="#">GitHub</a>
-					<a rel="noopener noreferrer" href="#">Discord</a>
-					<a rel="noopener noreferrer" href="#">Twitter</a>
-					<a rel="noopener noreferrer" href="#">YouTube</a>
-				</div>
+
 			</div>
 		</div>
-        <div class="flex-grow border-t border-[#F5F6FA] m-5"></div>
-		<div class="flex items-center justify-between px-6  text-sm">
-		<div class=" grid gap-4">
-           <img src="../../../assets/images/icons/logo.svg" alt="svg">
-            <span class="text-white font-semibold text-[12px]">© 2023 EATTE</span>
-        </div>
-           
-         <div class="flex gap-[20px]">
-             <i class='bx bxl-facebook-square  text-4xl' ></i>
-             <i class='bx bxl-linkedin-square text-4xl'></i>
-             <i class='bx bxl-instagram text-4xl' ></i>
+		<footer class="p-6 text-white z-10 relative bg-gradient-to-r from-[#FF6137] to-[#CC4D2C] ">
 
-         </div>
-		</div>
-	</footer>
-  </div>
+			<div class="container grid grid-cols-2 mx-auto gap-x-3 gap-y-8 sm:grid-cols-3 md:grid-cols-4">
+				<div class="flex flex-col space-y-4">
+					<h2 class=" font-semibold text-[26px] text-[#F5F6FA]">FEATURES</h2>
+					<div class="flex flex-col space-y-2 text-sm text-white">
+						<a rel="noopener noreferrer" href="#" class="footer_element">Order Management</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Takeout & Delivery Services</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Inventory Management</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Ingredients Management</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Customer Engagement</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Employee Management</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Back-Office Management</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Payment Processing</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Customer Support</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Point Of Sale (POS)</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Kitchen Display System (KDS)</a>
+					</div>
+				</div>
+				<div class="flex flex-col space-y-4">
+					<h2 class="f font-semibold text-[26px] text-[#F5F6FA]">RESTAURANT TYPES</h2>
+					<div class="flex flex-col space-y-2 text-sm text-white">
+						<a rel="noopener noreferrer" href="#" class="footer_element">Full Service Restaurant</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Food Truck</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Family Style</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Fine Dining</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Quick Service Restaurant</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Coffee Shop</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Fast Casual</a>
 
+						<a rel="noopener noreferrer" href="#" class="footer_element">Catering</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Bakery</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Cafe</a>
+					</div>
+				</div>
+				<div class="flex flex-col space-y-4">
+					<h2 class=" font-semibold text-[26px] text-[#F5F6FA]">COMPANY</h2>
+					<div class="flex flex-col space-y-2 text-sm text-white">
+						<a rel="noopener noreferrer" href="#" class="footer_element">About EATTE</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Why EATTE?</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Breakpoints</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Pricing</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Testimonials</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Contact us</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">Support</a>
+					</div>
+				</div>
+				<div class="flex flex-col space-y-4">
+					<h2 class=" font-semibold text-[26px]">Compare</h2>
+					<div class="flex flex-col space-y-2 text-sm text-white">
+						<a rel="noopener noreferrer" href="#" class="footer_element">toasttab</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">squareup</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">lightspeedhq</a>
+						<a rel="noopener noreferrer" href="#" class="footer_element">touchbistro</a>
+					</div>
+				</div>
+			</div>
+			<div class="flex-grow border-t border-[#F5F6FA] m-5"></div>
+			<div class="flex items-center justify-between px-6  text-sm">
+				<div class=" grid gap-4">
+					<img src="../../../assets/images/icons/logo.svg" alt="svg">
+					<span class="text-white font-semibold text-[12px]">© 2023 EATTE</span>
+				</div>
+
+				<div class="flex gap-[20px]">
+					<i class='bx bxl-facebook-square  text-4xl'></i>
+					<i class='bx bxl-linkedin-square text-4xl'></i>
+					<i class='bx bxl-instagram text-4xl'></i>
+
+				</div>
+			</div>
+		</footer>
+	</div>
 </template>
 
 <script>
@@ -83,12 +91,16 @@
 
 <style  scoped>
 .card {
-    background-color: #fff;
-    border-radius: 16px;
-    height: 300px;
-    margin-right: 250px;
-    margin-left: 250px;
-    margin-bottom: 10px;
+	background-color: red;
+	border-radius: 16px;
+	height: 300px;
+	margin-right: 250px;
+	margin-left: 250px;
+	margin-bottom: 10px;
+	position: absolute;
 }
 
+.footer_element {
+	@apply font-semibold text-base
+}
 </style>
