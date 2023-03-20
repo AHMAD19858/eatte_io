7cd3a0cdefeaab7b51d5faca78297b18f4cea707
Ahmad Essam <61994278+AHMAD19858@users.noreply.github.com>
2023-03-20 16:11:37 +0200

    forget password success alert

--- pages/forget-password.vue
+++ pages/forget-password.vue
@@ -69,14 +69,14 @@
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            style="fill: rgba(0, 0, 0, 1); transform: ; msfilter: "
+            style="fill: white; margin-inline: 5px; transform: ; msfilter: "
           >
             <path
               d="M20 2H4c-1.103 0-2 .894-2 1.992v12.016C2 17.106 2.897 18 4 18h3v4l6.351-4H20c1.103 0 2-.894 2-1.992V3.992A1.998 1.998 0 0 0 20 2zm-9 11.914-3.707-3.707 1.414-1.414L11 11.086l4.793-4.793 1.414 1.414L11 13.914z"
             ></path>
           </svg>
 
-          <p class="text-center text-white">
+          <p class="text-center text-white font-montse">
             {{ loginData.value.res.ForgotPasswordSuccess }}
           </p>
         </div>
